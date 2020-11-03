0eb7ef6f901102566fce8e0de8d4fab7d30dca72
JimY233 <54047662+JimY233@users.noreply.github.com>
2020-11-03 18:49:57 -0500

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -1,6 +1,6 @@
 import pytest
 import unittest
-from TwitterAPI import twitter_search
+from search import search
 
 class MyTestCase(unittest.TestCase):
     def test_num_successful(self):
@@ -14,12 +14,12 @@ class MyTestCase(unittest.TestCase):
         num = 1
 
         #res = twitter_search(keyword,num,consumer_key,consumer_secret,access_key,acess_secret):
-        res = twitter_search(keyword,num)
+        res = search(keyword,num)
                              
-        txt = []
-        for tweet in res:
-            txt.append(tweet.text)
-        print(txt)
+        #txt = []
+        #for tweet in res:
+        #    txt.append(tweet.text)
+        #print(txt)
 
 
         self.assertEqual(1, len(res))
