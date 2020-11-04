17f9993a9723782c1f9d9c99499e454767a2f668
JimY233 <54047662+JimY233@users.noreply.github.com>
2020-11-03 23:25:30 -0500

    Update TwitterAPI.py

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -23,10 +23,27 @@ def twitter_search(keyword,num):
 
     return text
 
+def twitter_search(userid,num):
+
+    #Twitter API credentials
+    consumer_key = os.getenv('CONSUMER_KEY')
+    consumer_secret = os.getenv('CONSUMER_SECRET')
+    access_key = os.getenv('ACCESS_KEY')
+    access_secret = os.getenv('ACCESS_SECRET')
+     
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    timeline = self.api.user_timeline(id=userid,count=num)
+
+    return timeline
+
 if __name__ == '__main__':
     
     keyword = input("Please enter the keyword:\n")
-    num = int(input("Please enter how many tweets you want to analyze:\n"))
+    userid = input("Please enter the userid:\n")
+    num = int(input("Please enter how many tweets you want to analyze:\n")
 
     twitter_search(keyword,num)
     
