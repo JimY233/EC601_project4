a142a5261751216de5b7905fd0ae1ec2096e41a9
JimY233 <54047662+JimY233@users.noreply.github.com>
2020-11-03 22:39:40 -0500

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -2,26 +2,17 @@ import pytest
 from search import search
 from TwitterAPI import twitter_search
 
-class MyTestCase():
-    def test_num_successful():
+def test_num_successful():
 
-        #consumer_key = os.getenv('CONSUMER_KEY')
-        #consumer_secret = os.getenv('CONSUMER_SECRET')
-        #access_key = os.getenv('ACCESS_KEY')
-        #access_secret = os.getenv('ACCESS_SECRET')
+    #consumer_key = os.getenv('CONSUMER_KEY')
+    #consumer_secret = os.getenv('CONSUMER_SECRET')
+    #access_key = os.getenv('ACCESS_KEY')
+    #access_secret = os.getenv('ACCESS_SECRET')
 
-        keyword = 'COVID-19'
-        num = 1
-
-        #res = twitter_search(keyword,num,consumer_key,consumer_secret,access_key,acess_secret):
-        res = search(keyword,num)
-                             
-        #txt = []
-        #for tweet in res:
-        #    txt.append(tweet.text)
-        #print(txt)
-
-        assert len(res) == 1
+    keyword = 'COVID-19'
+    num = 1
+    res = search(keyword,num)
+    assert len(res) == 1
 
 
 if __name__ == '__main__':
