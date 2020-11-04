2a56462fc8c449b03da3e7e3d1d8f75269c4e0e9
JimY233 <54047662+JimY233@users.noreply.github.com>
2020-11-03 23:04:53 -0500

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -2,16 +2,20 @@ import pytest
 from search import search
 from TwitterAPI import twitter_search
 
-def test_num_successful():
-
+def test_num1():
     #consumer_key = os.getenv('CONSUMER_KEY')
     #consumer_secret = os.getenv('CONSUMER_SECRET')
     #access_key = os.getenv('ACCESS_KEY')
     #access_secret = os.getenv('ACCESS_SECRET')
-
     keyword = 'COVID-19'
-    num = 1
+    num = 5
     res = search(keyword,num)
+    assert len(res) == 5
+    
+def test_num2():
+    keyword = 'COVID-19'
+    num = 5
+    res = twitter_search(keyword,num)
     assert len(res) == 1
 
 def test_type_successful():
