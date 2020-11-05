2621cec12df45493d558fc981199c1d293383d50
JimY233 <54047662+JimY233@users.noreply.github.com>
2020-11-04 20:15:28 -0500

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -44,10 +44,15 @@ def test_type():
     res = twitter_search(keyword,num)
     assert 'COVID-19' in res
     
-#def test_NLP():
+#def test_NLP1():
 #    text = 'Hello world!'
 #    sentiment = NLP_analyze(text)
 #    assert hasattr(sentiment,'score') == True
+
+#def test_NLP2():
+#    text = 'Hello world!'
+#    sentiment = NLP_analyze(text)
+#    assert hasattr(sentiment,'magnitude') == True
         
 if __name__ == '__main__':
     pytest.main()
